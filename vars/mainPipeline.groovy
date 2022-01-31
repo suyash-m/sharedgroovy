1b2b21751e797efb1d1ce6a05b15062ac7e53b00
suyash-m <69727351+suyash-m@users.noreply.github.com>
2022-01-31 22:25:05 +0530

    Update mainPipeline.groovy

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -1,6 +1,6 @@
 import src.org.sam.groov.*
 def call(){
-
+ checkout scm
 echo 'Loading pipeline definition'
 		Map pipelineDefinition = readYaml file: 'pipelineconfig.yml'
 pipelinetype= pipelineDefinition.pipelineType
