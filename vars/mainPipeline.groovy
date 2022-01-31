cd5a2a4164c551b93c6a69a583b4c033753bfc39
suyash-m <69727351+suyash-m@users.noreply.github.com>
2022-01-31 22:27:14 +0530

    Update mainPipeline.groovy

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -1,16 +1,14 @@
-import src.org.sam.groov.*
+
 def call(){
- checkout scm
-echo 'Loading pipeline definition'
-		Map pipelineDefinition = readYaml file: 'pipelineconfig.yml'
-pipelinetype= pipelineDefinition.pipelineType
-
-switch(pipelinetype) {
-
-    case 'java':
-    javaPipeline pipelineDefinition
-break;
-
-
-}
+  pipeline {
+      agent any
+      stages {
+        stage('Even Stage') {
+          steps {
+            echo "The build number is even"
+          }
+        }
+      }
+    }
+  }
 }
