f35491f4777051a691dfd9fd6ebe29394549bf30
suyash-m <69727351+suyash-m@users.noreply.github.com>
2022-01-31 22:42:32 +0530

    Update mainPipeline.groovy

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -2,18 +2,18 @@ imprt src.org.sam.groov
 
 def call(){
 
-echo 'Loading pipeline definition'
-Map pipelineDefinition = readYaml file: 'pipelineconfig.yml'
-	echo '${pipelineDefinition.pipelineType}'
-pipelinetype= pipelineDefinition.pipelineType
-
-switch(pipelinetype) {
-
-    case 'java':
-    javaPipeline pipelineDefinition
-   break;
-
+	pipeline{
+		
+		agent any
+		stages{
+			stage('Build'){
+				steps{
+					echo "Hello Build"
+			
+	
 }
-}
+			}
+		}
+	}
 
 
